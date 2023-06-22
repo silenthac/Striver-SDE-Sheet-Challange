e6be7e03af302f8f384c65d4670341d64b14a5fd
Prashant Pundir <69294779+silenthac@users.noreply.github.com>
2023-06-22 19:04:08 +0530

    Time: 743 ms (21.64%), Space: 295.4 MB (10.19%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+         vector<vector<int>>dp(s.size(),vector<int>(s.size()));
+        int count=0;
+        int ans=0;
+        string  res="00";
+        
+        for(int g=0;g<s.size();g++)
+        {
+            int i=0;
+            int j=g;
+            
+            while(j<s.size())
+            {
+                if(g==0)
+                {
+                    dp[i][j]=true;
+                    
+                }
+               
+                
+                
+                else if(g==1)
+                {
+                    if(s[i]==s[j])
+                    {
+                        dp[i][j] =true;
+                        
+                    }
+                    else
+                    {
+                        dp[i][j] =false;
+                    }
+
+                    
+                }
+            
+                
+                else
+                {
+                    if(s[i]==s[j]&&dp[i+1][j-1]==true)
+                    {
+                        dp[i][j] =true;
+                       
+                    }
+                    else
+                    {
+                        dp[i][j] =false;
+                    }
+                }
+                
+                if(dp[i][j]==true){
+                    count++;
+                    if((g+1)>ans)
+                    {
+                        ans =g+1;
+                        res = s.substr(j-g,g+1);
+                    }
+                }
+                
+                
+                i++;j++;
+                    
+                     
+            }
+               
+        }
+       // cout<<res;
+        
+       
+        
+            return res;
+        
+        
+    }
+};
