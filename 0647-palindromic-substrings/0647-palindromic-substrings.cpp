3354a072146b0f3f2b8621568dca631fca41ad64
Prashant Pundir <69294779+silenthac@users.noreply.github.com>
2023-06-22 17:34:21 +0530

    Time: 51 ms (21.64%), Space: 20.2 MB (21.44%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,40 +1,60 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        vector<vector<int>>dp(s.size(),vector<int>(s.size(),-1));
+        vector<vector<int>>dp(s.size(),vector<int>(s.size()));
         int count=0;
-        for(int i=0;i<s.size();i++)
+        
+        for(int g=0;g<s.size();g++)
         {
-            for(int j=i;j<s.size();j++)
+            
+            for(int i=0,j=g;j<s.size();i++,j++)
             {
-              count+=(solve(i,j,s,dp));
+                if(g==0)
+                {
+                    dp[i][j]=true;
+                    
+                }
+               
                 
                 
+                else if(g==1)
+                {
+                    if(s[i]==s[j])
+                    {
+                        dp[i][j] =true;
+                        
+                    }
+                    else
+                    {
+                        dp[i][j] =false;
+                    }
+
+                    
+                }
+            
+                
+                else
+                {
+                    if(s[i]==s[j]&&dp[i+1][j-1]==true)
+                    {
+                        dp[i][j] =true;
+                       
+                    }
+                    else
+                    {
+                        dp[i][j] =false;
+                    }
+                }
+                
+                if(dp[i][j]==true)
+                    count++;
+                    
+                     
             }
+               
         }
-        return count;
         
-    }
-    
-    
-    int  solve(int i,int j,string s,vector<vector<int>>&dp)
-    {
-        if(i>=j)
-            return 1;
+            return count;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-            
-            
-        
-        
-        if(s[i]==s[j])
-        {
-            return dp[i][j] = solve(i+1,j-1,s,dp);
-        }
-        else
-        {
-            return dp[i][j] =0;
-        }
     }
 };
