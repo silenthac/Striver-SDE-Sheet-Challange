1c89b5453747f02221dd9c43219f2307954daabf
Prashant Pundir <69294779+silenthac@users.noreply.github.com>
2023-06-30 11:49:42 +0530

    Time: 857 ms (77.59%), Space: 180.1 MB (91.61%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,4 +1,68 @@
-// class TrieNode{
+class TrieNode{
+    public:
+    
+    TrieNode* arr[2];
+    
+    TrieNode()
+    {
+        for(int i=0;i<2;i++)
+        {
+            arr[i] =NULL;
+        }
+    } 
+    
+    
+    
+    void insertit(int ele,TrieNode* root)
+    {
+        
+        for(int i=31;i>=0;i--)
+        {
+            int bit = (ele>>i)&1;
+            
+            if(root->arr[bit]==NULL)
+            {
+                root->arr[bit] = new  TrieNode();
+            }
+            
+            root = root->arr[bit];
+            
+        }
+    }
+    
+     int findmax(TrieNode* root,int ele)
+   {
+       int maxSum=0;
+       for(int i = 31;i>=0;i--)
+       {
+           int bit = (ele>>i)&1;
+           int findbit = !bit;
+           if(root->arr[findbit]!=NULL)
+           {
+               maxSum =maxSum|(1<<i);
+               root = root->arr[findbit];
+           }
+           else
+           {
+               root =root->arr[bit];
+           }
+       }
+       return maxSum;
+   }
+};
+    
+
+
+
+
+
+
+
+class Solution {
+public:
+    
+  
+//     class TrieNode{
 //     public:
     
 //     TrieNode* arr[2];
@@ -13,7 +77,7 @@
     
     
     
-//     void insert(int ele,TrieNode* root)
+//     void insertit(int ele,TrieNode* root)
 //     {
         
 //         for(int i=31;i>=0;i--)
@@ -51,70 +115,6 @@
 //        return maxSum;
 //    }
 // };
-
-
-
-
-
-
-
-class Solution {
-public:
-    
-  
-    class TrieNode{
-    public:
-    
-    TrieNode* arr[2];
-    
-    TrieNode()
-    {
-        for(int i=0;i<2;i++)
-        {
-            arr[i] =NULL;
-        }
-    } 
-    
-    
-    
-    void insertit(int ele,TrieNode* root)
-    {
-        
-        for(int i=31;i>=0;i--)
-        {
-            int bit = (ele>>i)&1;
-            
-            if(root->arr[bit]==NULL)
-            {
-                root->arr[bit] = new  TrieNode();
-            }
-            
-            root = root->arr[bit];
-            
-        }
-    }
-    
-    
-   int findmax(TrieNode* root,int ele)
-   {
-       int maxSum=0;
-       for(int i = 31;i>=0;i--)
-       {
-           int bit = (ele>>i)&1;
-           int findbit = !bit;
-           if(root->arr[findbit]!=NULL)
-           {
-               maxSum =maxSum|(1<<i);
-               root = root->arr[findbit];
-           }
-           else
-           {
-               root =root->arr[bit];
-           }
-       }
-       return maxSum;
-   }
-};
     int findMaximumXOR(vector<int>& nums) {
         TrieNode* root = new TrieNode();
         TrieNode* temp =root;
