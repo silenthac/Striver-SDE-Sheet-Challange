d8f3b1dbd3cc93689e5779424efee1bdb0eafadc
Prashant Pundir <69294779+silenthac@users.noreply.github.com>
2023-08-03 08:11:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.40%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<string>ans;
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0)
+        {
+            return ans;
+        }
+        unordered_map<int,string>mp;
+        mp[2] = "abc";
+         mp[3] = "def";
+        mp[4] = "ghi";
+         mp[5] = "jkl";
+         mp[6] = "mno";
+         mp[7] = "pqrs";
+         mp[8] = "tuv";
+         mp[9] = "wxyz";
+        
+        solve(0,digits,mp,"");
+        return ans;
+        
+             
+            
+        
+    }
+    
+    void solve(int index,string &digits,unordered_map<int,string>&mp,string temp)
+    {
+        if(temp.size()==digits.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        
+        for(int i = index;i<digits.size();i++)
+        {
+          int t =  int(digits[i]) - 48;
+            for(auto it:mp[t])
+            {
+                solve(i+1,digits,mp,temp+it);
+            }
+            
+            
+        }
+    }
+};
