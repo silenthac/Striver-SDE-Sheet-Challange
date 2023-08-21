34be9637b3b0366f0a1f1ca30620c8cc891c4c9d
Prashant Pundir <69294779+silenthac@users.noreply.github.com>
2023-08-21 10:55:39 +0530

    Time: 50 ms (29.14%), Space: 91.7 MB (20.17%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -6,7 +6,8 @@ public:
         for(int i=1;i<=n/2;i++)
         {
 
-                
+                if(n%i==0)
+                {
                 string temps = s.substr(0,i);
                 
                 string reps ="";
@@ -18,6 +19,7 @@ public:
                 if(reps==s)
                 {
                     return true;
+                }
                 }
             
             
